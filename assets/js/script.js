9b2bc2fc3211bb73fa04b650b63588435c1f7752
jafet <jafet.manauth@hotmail.com>
2021-05-16 23:59:57 -0500

    finished function that generates all of the user's input into a paragraph

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,61 @@
 var makeMagic = document.querySelector("#magicButton")
 
 function convert(){
-    var Txt1 = document.getElementById("politeText").value;
-    var Txt2 = document.getElementById("empathyText").value;
-    var Txt3 = document.getElementById("toutingBenefitsText").value;
-    var Txt4 = document.getElementById("smallTalkText").value;
+//asigns all of the drop down choices and written paragraphs to variables
     var yesNo1 = document.getElementById("politeness").value;
     var yesNo2= document.getElementById("empathy").value;
     var yesNo3 = document.getElementById("toutingBenefits").value;
     var yesNo4 = document.getElementById("smallTalk").value;
+    var Txt1 = document.getElementById("politeText").value;
+    var Txt2 = document.getElementById("empathyText").value;
+    var Txt3 = document.getElementById("toutingBenefitsText").value;
+    var Txt4 = document.getElementById("smallTalkText").value;
+//this loops through all of the children of the form and makes them dissapear
+    var x = document.getElementById("container1").children;
     
+        for (i=0; i < x.length; i++){
+         x[i].style.display = "none";
+        };
+    
+    var d = document.createElement("div");
+    
+    d.setAttribute("id", "finalBlock");
+    
+    document.getElementById("container1").appendChild(d);
+    
+    var h1= document.createElement("h3");
+    var h2= document.createElement("h3");
+    var h3= document.createElement("h3");
+    var h4= document.createElement("h3");
+
+    var p1= document.createElement("p");
+    var p2= document.createElement("p");
+    var p3= document.createElement("p");
+    var p4= document.createElement("p");
+
+    document.getElementById("finalBlock").appendChild(h1);
+    document.getElementById("finalBlock").appendChild(p1);
+
+    document.getElementById("finalBlock").appendChild(h2);
+    document.getElementById("finalBlock").appendChild(p2);
+
+    document.getElementById("finalBlock").appendChild(h3);
+    document.getElementById("finalBlock").appendChild(p3);
+
+    document.getElementById("finalBlock").appendChild(h4);
+    document.getElementById("finalBlock").appendChild(p4);
+
+    h1.textContent = "Politeness - " + yesNo1;
+    p1.textContent = Txt1;
+
+    h2.textContent = "Empathy - " + yesNo2;
+    p2.textContent = Txt2;
+
+    h3.textContent = "Touting Benefits - " + yesNo3;
+    p3.textContent = Txt3;
+
+    h4.textContent = "Small Talk - " + yesNo4;
+    p4.textContent = Txt4;
 }
 
 
