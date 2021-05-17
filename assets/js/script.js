885eba5a634c326015eab236a8a79cd766a25cc5
jafet <jafet.manauth@hotmail.com>
2021-05-16 20:49:52 -0500

    added button and started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//when I click convert
+//then all sub principal choices and text areas get turned into one single paragraph 
+//i am able to copy each principal individually onto clipboard
+
+var makeMagic = document.querySelector("#magicButton")
+
+function convert(){
+    console.log('sup foo');
+}
+
+
+makeMagic.addEventListener("click", convert)
